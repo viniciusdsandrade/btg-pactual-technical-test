2a12aeb330ff99662df78e8c94356645c3e6f43f
andrade <viniciusdsandrade0662@gmail.com>
2025-12-04 22:17:44 -0300

    setup:

---
+++ src/main/kotlin/com/restful/btg/pactual/technical/test/BtgPactualTechnicalTestApplication.kt
@@ -0,0 +1,11 @@
+package com.restful.btg.pactual.technical.test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BtgPactualTechnicalTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<BtgPactualTechnicalTestApplication>(*args)
+}
